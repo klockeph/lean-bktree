ee64868913dbe4e487dd9b2f53ea7bb2a1e2925b
Philipp Klocke <Phil_K97@gmx.de>
2023-08-05 23:27:06 +0200

    Mark functions as `partial` to compile.
    Note: They're not partial. I'm just too stupid to write termination
    proofs.

--- BkTree.lean
+++ BkTree.lean
@@ -7,7 +7,7 @@ class metric (α : Type _) :=
 instance int_metric : metric Int where
   distance x y := Int.natAbs (x-y)
 
-def levenshtein (x : List α) (y : List α) [BEq α] : Nat :=
+partial def levenshtein (x : List α) (y : List α) [BEq α] : Nat :=
   match (x, y) with
   | ([],[]) => 0
   | ([],y) => y.length
@@ -15,7 +15,7 @@ def levenshtein (x : List α) (y : List α) [BEq α] : Nat :=
   | ((x::xs),(y::ys)) => if x == y then levenshtein xs ys
   else 1 + min3 (levenshtein xs (y::ys)) (levenshtein (x::xs) ys) (levenshtein xs ys)
 where min3 (a : Nat) (b : Nat) (c : Nat) : Nat := min (min a b) c
--- TODO: prove termination
+-- TODO: prove termination and remove `partial`.
 
 instance levenshtein_metric {α: Type} [BEq α] : metric (List α) where
   distance x y := levenshtein x y
@@ -55,7 +55,7 @@ def BkTree.singleton (v : β) : BkTree β d :=
   BkTree.node v Lean.AssocList.nil
 
 -- inserts a new element into a bk tree
-def BkTree.insert (t : BkTree β d) (v : β) : BkTree β d :=
+partial def BkTree.insert (t : BkTree β d) (v : β) : BkTree β d :=
   match t with
   | leaf => singleton v
   | node val cs => 
@@ -64,10 +64,10 @@ def BkTree.insert (t : BkTree β d) (v : β) : BkTree β d :=
     else match cs.find? dist with
     | none => node val (cs.insert dist (singleton v))
     | some c => node val (cs.replace dist (c.insert v))
--- TODO: prove termination
+-- TODO: prove termination and remove `partial`.
 
 -- checks if the BkTree contains an element
-def BkTree.contains (t : BkTree β d) (v : β) : Bool :=
+partial def BkTree.contains (t : BkTree β d) (v : β) : Bool :=
   match t with
   | leaf => false
   | node val cs =>
@@ -76,7 +76,7 @@ def BkTree.contains (t : BkTree β d) (v : β) : Bool :=
     else match cs.find? dist with
     | none => false
     | some c => c.contains v
--- TODO: prove termination
+-- TODO: prove termination and remove `partial`.
 
 -- Returns a new AssocList that contains all elements with keys between min and max.
 -- TODO: generalize to work with any key [k : LE] instead of Nat. Gave a Prop instead of a Bool though..
@@ -87,7 +87,7 @@ def Lean.AssocList.narrow (l : Lean.AssocList Nat c) (min: Nat) (max: Nat) : (Le
   | Lean.AssocList.cons x y ls => if (min <= x) && (x <= max) then Lean.AssocList.cons x y (ls.narrow min max) else (ls.narrow min max)
 
 -- Find all elements that are at most `dist` away from `v`.
-def BkTree.near (t : BkTree β d) (dist : Nat) (v : β) : (List β) :=
+partial def BkTree.near (t : BkTree β d) (dist : Nat) (v : β) : (List β) :=
   match t with
   | leaf => []
   | node val cs => 
@@ -96,9 +96,9 @@ def BkTree.near (t : BkTree β d) (dist : Nat) (v : β) : (List β) :=
   let subtrees := sublist.toList.map Prod.snd;
   let rest := subtrees.foldl (fun x y => x ++ y.near dist v) []
   if (d <= dist) then val::rest else rest
--- TODO: prove termination.
+-- TODO: prove termination and remove `partial`.
 
-def findClosest (v : β) (candidate : (β × Nat)) (t : BkTree β d) : (β × Nat) :=
+partial def findClosest (v : β) (candidate : (β × Nat)) (t : BkTree β d) : (β × Nat) :=
   match t with
   | BkTree.leaf => candidate
   | BkTree.node val cs => 
@@ -107,7 +107,7 @@ def findClosest (v : β) (candidate : (β × Nat)) (t : BkTree β d) : (β × Na
     let sublist := cs.narrow (candidate.snd - dist - 1) (candidate.snd + dist + 1);
     let subtrees := sublist.toList.map Prod.snd
   subtrees.foldl (findClosest v) new_cand
--- TODO: prove termination.
+-- TODO: prove termination and remove `partial`.
 
 -- Returns the closest element to a given value.
 def BkTree.closest? (t : BkTree β d) (v : β) : Option (β × Nat) :=
@@ -119,15 +119,14 @@ instance lev_string : metric String where
   distance x y := levenshtein (x.toList) (y.toList)
 
 
-#check (BkTree.leaf : (BkTree Int int_metric))
-#eval ((BkTree.leaf.insert 3).insert 1 : (BkTree Int int_metric)).closest? 2
+-- #check (BkTree.leaf : (BkTree Int int_metric))
+-- #eval ((BkTree.leaf.insert 3).insert 1 : (BkTree Int int_metric)).closest? 2
 
+-- abbrev example_tree : (BkTree String lev_string) := 
+--   (BkTree.leaf.insert "cat"
+--             |>.insert "dog"
+--             |>.insert "house")
 
-abbrev example_tree : (BkTree String lev_string) := 
-  (BkTree.leaf.insert "cat"
-            |>.insert "dog"
-            |>.insert "house")
-
-#eval example_tree.toList
-#eval example_tree.near 4 "mouse"
-#eval example_tree.closest? "mouse"
+-- #eval example_tree.toList
+-- #eval example_tree.near 4 "mouse"
+-- #eval example_tree.closest? "mouse"
